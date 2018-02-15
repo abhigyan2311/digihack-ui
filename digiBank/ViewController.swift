5baa1645487861579ebfc34fcf7aa7c6b3d79c85
Abhigyan Singh <abhigyan2311@gmail.com>
2018-02-15 20:28:16 +0530

    Project Initialize

---
+++ digiBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  digiBank
+//
+//  Created by Abhigyan Singh on 15/02/18.
+//  Copyright © 2018 DBS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
